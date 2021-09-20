687cb9c2abea4e830fd2f87caf6982c19a0b3b8b
navee-ramesh <54928117+naveeramesh@users.noreply.github.com>
2021-09-20 21:37:54 +0530

    Dart data types

---
+++ data_types.dart
@@ -0,0 +1,24 @@
+//DartDatatypes - represents the attribute of the data 
+//specifies the type of value
+//int,string,boolean,list,maps
+//INT
+int num = 2;
+// String
+String name="Ramesh";
+// Boolean
+bool language=true;
+bool not_programming_language=false;
+void main(){
+  // List
+  List num=[1,2,3,4,5,6,7,8,9,10];
+    print(num);
+  List name=["dart","Flutter"];
+  print(name);
+  // Maps
+  Map places=new Map();
+// places[firstplace]="Telungana";
+places['name']="Telungana";
+places['name2']="Chennai";
+print(places);
+}
+
