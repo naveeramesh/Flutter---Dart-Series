237aba17f46181ebdfb28504f830f6a9f05658f4
navee-ramesh <54928117+naveeramesh@users.noreply.github.com>
2021-09-21 19:44:17 +0530

    Variable

---
+++ What is dart datatype/variable.dart
@@ -0,0 +1,31 @@
+// A variable is “space in the memory”, also known 
+//as indentifiers.
+//Following are the naming rules for an variable :
+// 1.Variable cannot be keywords.
+// 2.Variable cannot contain spaces and special characters, 
+//except the underscore (_) and the dollar ($) sign.
+// 3.Variable names cannot begin with a number.
+
+// Syntax:
+void main(){
+  var name="Syntax";
+  print(name);
+  }
+// Type annotations:
+void main(){
+ late int num2;
+  print(num2);
+  int num=2;
+  print(num);
+}
+// final,constant,dynamic
+//Syntax:
+final name="You tube";
+const pi;
+void main(){
+ dynamic  num=2;
+  //you can also change the value later
+  print(num);
+  }
+//You cannot use all the 3 main function make sure you comment other two when you run on of these.
+
