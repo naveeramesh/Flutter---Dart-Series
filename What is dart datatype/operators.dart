1fe459e53dc5fdbf02bcae30b41f228ad19cdbc7
navee-ramesh <54928117+naveeramesh@users.noreply.github.com>
2021-09-26 11:09:50 +0530

    Create operators.dart

---
+++ What is dart datatype/operators.dart
@@ -0,0 +1,47 @@
+ //Operators
+// evaluates the value 
+//1. Arithmetic operators
+//+,-,*,/,~/,%
+// void main(){
+//   int a=10;
+//   int b=8;
+//   print( a % b);
+// }
+//2. relational operator
+//>,<,<=,>=,==,!=
+// void main(){
+//   int a =5;
+//   int b =2;
+//   print( a != b);
+// }
+// 3. Type test operator
+// is , !is
+// void main(){
+//   String a="Hello";
+//   print(a is !String);
+// }
+// 4. Bitwise operators
+//&,|,^,NOT,<<,>>
+// void main(){
+//   int a=5;
+//   int b=7;
+//   int result= a & b;
+//   print(result);
+  
+// }
+// 1 0 1
+// 0 1 1 1
+// 101
+//5
+// 5. Assignment operator
+// =,??=
+//6. Logical operators
+//&& ,||,!
+// 7. Conditional operators
+// syntax = (condition)? True:False
+void main(){
+  String a ="Hello";
+  print( a == "Hello" ? "Yes": "No");
+}
+
+
