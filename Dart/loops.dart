24fc5f6503ac548638ccd546eade3a6f9f8f2a15
navee-ramesh <54928117+naveeramesh@users.noreply.github.com>
2021-09-26 12:09:07 +0530

    Create loops.dart

---
+++ Dart/loops.dart
@@ -0,0 +1,78 @@
+//loops
+//what is loop?
+// loops are blovk of code which executes again and again 
+//until it reach some specific condition.
+// for loop
+// for each loop
+// for in loop
+// while loop
+// do while loop
+// 1. For loop
+//Syntax:
+// for (initialization;condition;increment){
+//   //block of code
+// }
+// Flow
+// 1. initialization
+//2.condition
+//3.=body of foer loop(code)
+//4.increment
+// void main(){
+//   for (int i =0;i<10;i++){
+//     print(i);
+//   }
+// }
+// for in loop:
+//Syntax:
+// void main(){
+//   List num=[1,2,3,4];
+//   for (int i in num){//i=1
+//     print(i);
+//   }
+// }
+//for each loop
+//Syntax:
+// collection.for each(void f(value))
+
+// void main(){
+//   var name=[1,2,3,4];
+//   name.forEach((var name)=>print(name));
+// }
+
+// void main(){
+//   var num=[10,20,30,40];
+//   num.forEach((var num)=>print(num));
+// }
+
+// While loop:
+//Syntax
+// while(condition){
+//body;
+//increment;
+//}
+
+// void main(){
+//   var num=4;
+//   int i=0;
+//   while(i<=num){
+//     print(i);
+//     i++;
+//   }
+// }
+// do while 
+
+// do{
+//   body;
+//i++;
+// }
+// while(condiiton);
+// void main(){
+//      var num=4;
+//      int i=0;
+//   do{
+//     print(i);
+//     i++;
+//   }
+//   while(i<num);
+// }
+
