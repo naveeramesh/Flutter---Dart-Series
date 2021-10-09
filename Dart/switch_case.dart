242a91b4b61a69bddd7787594431ee1e06e11b91
navee-ramesh <54928117+naveeramesh@users.noreply.github.com>
2021-10-10 00:28:16 +0530

    Create switch_case.dart

---
+++ Dart/switch_case.dart
@@ -0,0 +1,23 @@
+// Switch Case
+//simplied form of nested if
+//eg
+// void main(){
+//   String bg="O";
+//   switch(bg){
+//     case "O":{
+//         print("Universal bg");
+//         break;
+//       }
+//     case "A":{
+//         print("A bg");
+//         break;
+//       }
+//     case "B":{
+//         print("B bg");
+//         break;
+//       }
+//     default:{
+//       print("No bg");
+//     }
+//   }
+// }
